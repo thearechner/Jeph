60c185112b11764084dc3bb131899b15e0134fb0
thearechner <theaemilia@gmail.com>
2018-09-21 16:06:55 +0200

    Add media queries for main content

---
+++ resources/JS/main.js
@@ -0,0 +1,12 @@
+// A $( document ).ready() block . jQuery
+$( document ).ready(function() {
+  // All other jQuery code goes inside here
+   // $(window).scroll(function () {
+   //    if ($(this).scrollTop() > 50) {
+   //       $(‘body’).addClass(‘changeColor’)
+   //    }
+   //    if ($(this).scrollTop() < 50) {
+   //       $(‘body’).removeClass(‘changeColor’)
+   //    }
+   //  });
+});
