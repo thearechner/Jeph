9b33c07781e325c13bf4498524bd3326d236430b
thearechner <theaemilia@gmail.com>
2018-11-23 15:55:24 +0100

    Add background image and colour changes with scollmagic

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,12 +1,32 @@
-// A $( document ).ready() block . jQuery
-$( document ).ready(function() {
-  // All other jQuery code goes inside here
-   // $(window).scroll(function () {
-   //    if ($(this).scrollTop() > 50) {
-   //       $(‘body’).addClass(‘changeColor’)
-   //    }
-   //    if ($(this).scrollTop() < 50) {
-   //       $(‘body’).removeClass(‘changeColor’)
-   //    }
-   //  });
+$(document).ready(function(){
+
+  // Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+  // Scene for background color
+  var bgColorScene = new ScrollMagic.Scene({
+    triggerElement: 'body .second-paragraph',
+    // duration: '300%',
+    triggerHook: 0
+  })
+  .setClassToggle('body', 'changeColor') // add class changeColor to body element
+
+  .addIndicators({
+    name: 'change color'
+  }) // adds debug info
+  .addTo(controller);
+
+  // Scene for pinning the background image
+  var pinBackgroundImage = new ScrollMagic.Scene({
+    triggerElement: 'body .second-paragraph',
+    duration: '100%',
+    triggerHook: 0.9
+  })
+  .setClassToggle('.background-image', 'changeImage')
+  .setPin('.background-image .changeImage')
+  .addIndicators({
+    name: 'pin image'
+  }) // adds debug info
+  .addTo(controller);
+
 });
