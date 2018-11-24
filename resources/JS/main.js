b6ece8c885e12ab643cc498fa39f3e6ab5894937
thearechner <theaemilia@gmail.com>
2018-11-24 10:53:43 +0100

    Remove add indicators for scrollmagic debugging

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -5,28 +5,28 @@ $(document).ready(function(){
 
   // Scene for background color
   var bgColorScene = new ScrollMagic.Scene({
-    triggerElement: 'body .second-paragraph',
+    triggerElement: 'body .trigger-image',
     // duration: '300%',
     triggerHook: 0
   })
   .setClassToggle('body', 'changeColor') // add class changeColor to body element
 
-  .addIndicators({
-    name: 'change color'
-  }) // adds debug info
+  // .addIndicators({
+  //   name: 'change color'
+  // }) // adds debug info
   .addTo(controller);
 
   // Scene for pinning the background image
   var pinBackgroundImage = new ScrollMagic.Scene({
-    triggerElement: 'body .second-paragraph',
+    triggerElement: 'body .trigger-image',
     duration: '100%',
     triggerHook: 0.9
   })
   .setClassToggle('.background-image', 'changeImage')
   .setPin('.background-image .changeImage')
-  .addIndicators({
-    name: 'pin image'
-  }) // adds debug info
+  // .addIndicators({
+  //   name: 'pin image'
+  // }) // adds debug info
   .addTo(controller);
 
 });
